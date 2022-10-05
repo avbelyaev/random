2562ab99d194e167da61f699095596999343291b
Anthony Belyaev <anthony.belyaev@vonage.com>
2022-10-05 23:20:53 +0400

    Update AuthView.swift

--- tru-id/ios-app/Shared/AuthView.swift
+++ tru-id/ios-app/Shared/AuthView.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 
 struct AuthView: View {
-
+    
     @EnvironmentObject var authChecker: AuthChecker
-
+    
     @State private var name: String = ""
     @State private var number: String = ""
     @State private var authLogs: String = ""
-
+    
     var body: some View {
-
+        
         Text("Hello, \(name)")
             .font(.title)
         Text("Enter your name and number to login")
-
-
-
+        
+        
+        
         VStack {
             TextField(
                 "Given Name",
@@ -34,34 +34,34 @@ struct AuthView: View {
                 text: $number
             ).keyboardType(.numberPad)
         }.textFieldStyle(.roundedBorder)
-
-
-
-
+        
+        
+        
+        
         Button("Login", action: {
             print("button is pressed")
             Task {
                 print("running the task")
-
-                let authInitResult = await authChecker.init2FA(phoneNumber: "447840111222")
+                
+                let authInitResult = await authChecker.init2FA(phoneNumber: "447840197888")
 //                authLogs += "Check{id=\(authChecker.authInitResult.check_id), url=\(authChecker.authInitResult.check_url)}, "
                 print("---")
                 print(authInitResult)
-
+                
                 DispatchQueue.main.async {
                     authLogs = "res=\(authInitResult)"
                 }
             }
         })
-
-
-
+        
+        
+        
         Text(
             "Auth check progress: \(authLogs)"
         ).frame(width: 300)
-
+        
     }
-
+    
 }
 
 struct AuthView_Previews: PreviewProvider {
