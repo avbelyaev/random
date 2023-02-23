d899db98cca824aa71aba0b1b7f93185dad29c37
Anthony Belyaev <anthony.belyaev@vonage.com>
2023-02-23 18:42:45 +0600

    vue vite

---
+++ vue-facebook-buttons/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import FacebookButtons from "@/components/FacebookButtons.vue";
+</script>
+
+<template>
+  <div id="app">
+    <main>
+      <FacebookButtons/>
+    </main>
+  </div>
+</template>
---
+++ vue-facebook-buttons/src/components/FacebookButtons.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-facebook-login app-id="966242223397117"></v-facebook-login>
+</template>
+
+<script>
+import VFacebookLogin from 'vue-facebook-login-component'
+
+export default {
+  name: 'FacebookButtons',
+
+  components: {
+    VFacebookLogin,
+  },
+}
+</script>
